a6b0fb759c04ccc110a938faa9edf4cfb5389e7c
lauragpse <lauragpse446@gmail.com>
2023-08-22 22:14:04 -0300

    new

--- fundamentosJS/organização.js
+++ fundamentosJS/organização.js
@@ -51,5 +51,7 @@ const total = avaliacao1 * peso1 + avaliacao2 * peso2
 const media = total / (peso1 + peso2)
 
 console.log(media.toFixed(2))
-console.log(media.toString(2))
+console.log(media.toString(2)) //em binario
+console.log(typeof media)
+console.log(typeof Number)
 
