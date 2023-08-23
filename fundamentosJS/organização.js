f0aeeb73e07251f05ec304f77f00f07e9abd1964
lauragpse <lauragpse446@gmail.com>
2023-08-22 22:05:26 -0300

    new

--- fundamentosJS/organização.js
+++ fundamentosJS/organização.js
@@ -7,3 +7,32 @@ console.log("sentença de código");
     }
 }
 
+/* comentario de 
+multiplas linhas */
+
+/*
+* outra maneira de 
+* comentar
+*/
+
+
+//variaveis e constantes:
+
+var a = 3 //variavel de identificador 'a' recebe o valor de atribuição '3', --recomendado
+
+const c = 5 //constante +++ recomendado
+
+let b = 4 // +- recomendado
+
+ console.log(a, b, c);
+
+
+ let qualquer = 'legal'
+ console.log(qualquer)
+ console.log(typeof qualquer) //diz o nome da linguagem
+
+ qualquer = 3.545
+ console.log(qualquer)
+ console.log(typeof qualquer)
+
+
