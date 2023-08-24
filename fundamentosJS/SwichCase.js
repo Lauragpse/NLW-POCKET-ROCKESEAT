8185c24c3d874021067418a6af6a635a41695a12
lauragpse <lauragpse446@gmail.com>
2023-08-23 21:03:57 -0300

    js

--- fundamentosJS/SwichCase.js
+++ fundamentosJS/SwichCase.js
@@ -1,17 +1,4 @@
-// if e else
-
-const nota = 85;
-if (nota >=90) {
-    console.log("nota A");
-}   else if (nota >=80) {
-        console.log("nota B");
-}   else if (nota >= 70) {
-    console.log("Nota C");
-}   else {
-    console.log("Nota abaixo de C");
-    }
-
-    //switch case
+//switch case
 
     const diaDaSemana = "quarta";
     switch (diaDaSemana) {
