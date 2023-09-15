dad62212809f1cad85035bb5c2e3740d16e44b73
lauragpse <lauragpse446@gmail.com>
2023-09-15 17:32:04 -0300

    js

---
+++ atividadesJS/objetoCarro.js
@@ -0,0 +1,15 @@
+let carro = {
+    marca: "toyota",
+    modelo: "corolla",
+    ano: 2020
+
+}
+
+function exibirDetalhes(){
+    console.log(`Marca: ${carro.marca}`);
+    console.log(`Modelo: ${carro.modelo}`);
+    console.log(`Ano: ${carro.ano}`);
+}
+
+exibirDetalhes();
+
