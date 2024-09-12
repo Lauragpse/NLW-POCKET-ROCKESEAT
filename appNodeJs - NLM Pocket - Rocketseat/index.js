162a8ac0b6c081cc021c804bd3c7918439518ba9
Laura Leite <lauragpse446@gmail.com>
2024-09-12 01:32:37 -0300

    y

--- appNodeJs - NLM Pocket - Rocketseat/index.js
+++ appNodeJs - NLM Pocket - Rocketseat/index.js
@@ -1,5 +1,11 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
+
+let meta = {
+    value: 'tomar 2L de agua por dia',
+    checked: false
+}
+let metas = [meta,]
 const cadastarMeta = async () => {
     const meta = await input({ message: 'Digite a meta: ' })
 
@@ -7,6 +13,47 @@ const cadastarMeta = async () => {
         console.log('A meta não pode ser vazia')
         return
     }
+
+    metas.push({ value: meta, checked: false })
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: 'use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa.',
+        choices: [...metas],
+        instructions: false,
+    });
+    metas.forEach((m) => {
+        m.checked = false
+    })
+    if (respostas.length == 0) {
+        console.log('nenhuma meta selecionada!');
+        return;
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta;
+        });
+        meta.checked = true
+    });
+    console.log('Meta(s) marcadas como concluida(s)')
+};
+
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if (realizadas.length == 0) {
+        console.log('Não existem metas realizadas :(')
+        return
+    }
+
+    await select({
+        message: 'Metas Realizadas',
+        choices: [...realizadas]
+    })
 }
 
 const start = async () => {
@@ -23,6 +70,10 @@ const start = async () => {
                     value: 'listar'
                 },
                 {
+                    name: 'Metas realizadas',
+                    value: 'realizadas'
+                },
+                {
                     name: 'Sair',
                     value: 'sair'
                 }
@@ -32,9 +83,13 @@ const start = async () => {
         switch (opcao) {
             case 'cadastrar':
                 await cadastarMeta()
+                console.log(metas)
                 break
             case 'listar':
-                console.log('Vamos listar')
+                await listarMetas()
+                break
+            case 'realizadas':
+                await metasRealizadas()
                 break
             case 'sair':
                 console.log('Até a próxima')
