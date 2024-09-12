2d30956e7238a102dc232c47cd000bc698dd0b99
Laura Leite <111168549+Lauragpse@users.noreply.github.com>
2024-09-11 23:18:58 -0300

    Delete atividadesJS directory

--- atividadesJS/gerarNumeros.js
+++
@@ -1,9 +0,0 @@
-function gerarSenha() {
-    const senha = Math.round(Math.random() * (999999 - 100000) + 100000)
-    //console.log(senha)
-    return senha
-}
-
-const senha = gerarSenha()
-console.log(senha)
-//gerarSenha()
--- atividadesJS/temperatura.js
+++
@@ -1,6 +0,0 @@
-function temperatura(celsius) {
-    const Fahrenheit = (celsius * 9 / 5) + 32;
-    return Fahrenheit
-}
-const celsius = 30;
-console.log(temperatura(celsius))
